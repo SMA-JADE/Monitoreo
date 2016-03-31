662837068648deed0ed2299b73eb1e5549739c42
Juan P. Soto <jpsotob@gmail.com>
2016-03-30 18:06:28 -0700

    Create Ag_interfaz.java

---
+++ Ag_interfaz.java
@@ -0,0 +1,8 @@
+import jade.core.Agent;
+
+public class Ag_interfaz extends Agent {
+  protected void setup (){
+    // Líneas de código aquí
+    // Recibe las alertas del Ag_coordinador y se las hace llegar al usuario (Envía un mensaje a la consola).
+  }
+}
