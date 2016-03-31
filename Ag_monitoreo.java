db4810392b8351797c294c265d6929a6f6be85c5
Juan P. Soto <jpsotob@gmail.com>
2016-03-30 18:05:06 -0700

    Create Ag_monitoreo.java

---
+++ Ag_monitoreo.java
@@ -0,0 +1,9 @@
+import jade.core.Agent;
+
+public class Ag_monitoreo extends Agent {
+  protected void setup (){
+    // Líneas de código aquí
+    // Este agente debe checar cada 5 segundos la temperatura (simular los cambios de temperatura).
+    // Además debe enviar el valor de la temperatura al Ag_coordinador.
+  }
+}
