28a6afed23addf0bfd215f768a171d082fe77d79
Ernesto Salazar <nethojs29@gmail.com>
2016-03-31 12:43:27 -0700

    Ya quedo, solo que checa cada medio segundo con fines didactico :D

--- Ag_coordinador.java
+++ Ag_coordinador.java
@@ -1,9 +1,30 @@
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class Ag_coordinador extends Agent {
+
   protected void setup (){
     // Líneas de código aquí
     // Debe escuchar constantemente al Ag_monitor.
     // y deliberar si es conveniente lanzar una alerta o no al Ag_interfaz
+      addBehaviour(new CyclicBehaviour() {
+          @Override
+          public void action() {
+              ACLMessage msg = receive();
+              if(msg != null){
+                  int temp = Integer.parseInt(msg.getContent());
+                  if(temp > 40){
+                      ACLMessage alert = new ACLMessage(ACLMessage.CONFIRM);
+                      alert.setContent("Alerta, evitar actividades al aire libre");
+                      alert.addReceiver(new AID("inter",false));
+                      send(alert);
+                  }
+              }else{
+
+              }
+          }
+      });
   }
 }
--- Ag_monitoreo.java
+++ Ag_monitoreo.java
@@ -1,9 +1,31 @@
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.lang.acl.ACLMessage;
+
+import java.util.Random;
 
 public class Ag_monitoreo extends Agent {
+
+  Random random;
+  int temp;
+
   protected void setup (){
     // Líneas de código aquí
     // Este agente debe checar cada 5 segundos la temperatura (simular los cambios de temperatura).
     // Además debe enviar el valor de la temperatura al Ag_coordinador.
+    random = new Random();
+    addBehaviour(new TickerBehaviour(this,500) {
+        @Override
+        protected void onTick() {
+            temp = random.nextInt(50);
+            System.out.println(""+temp);
+            ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+            msg.setContent(""+temp);
+            msg.addReceiver(new AID("coor",false));
+            send(msg);
+        }
+    });
   }
 }
