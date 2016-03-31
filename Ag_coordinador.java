46b027af4de84d46a79aa2cb72ac5fe702e484ce
Juan P. Soto <jpsotob@gmail.com>
2016-03-30 18:03:07 -0700

    Create Ag_coordinador.java

---
+++ Ag_coordinador.java
@@ -0,0 +1,9 @@
+import jade.core.Agent;
+
+public class Ag_coordinador extends Agent {
+  protected void setup (){
+    // Líneas de código aquí
+    // Debe escuchar constantemente al Ag_monitor.
+    // y deliberar si es conveniente lanzar una alerta o no al Ag_interfaz
+  }
+}
