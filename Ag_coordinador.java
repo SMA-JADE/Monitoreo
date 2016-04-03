38bcf4910eb1ca185bfe253fba852ffb8d9d48be
Erick Fdo. Lopez Fimbres <y3rick_18@hotmail.com>
2016-04-03 14:36:27 -0700

    Ya la pasa al Ag_interfaz

--- Ag_coordinador.java
+++ Ag_coordinador.java
@@ -1,9 +1,37 @@
+/**
+ *
+ * @author Erick
+**/
+package agentes;
+
+import jade.core.AID;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage; 
 import jade.core.Agent;
 
+
 public class Ag_coordinador extends Agent {
-  protected void setup (){
-    // Líneas de código aquí
-    // Debe escuchar constantemente al Ag_monitor.
-    // y deliberar si es conveniente lanzar una alerta o no al Ag_interfaz
-  }
+    public final static String Alerta1 = " es soportable, aun as� ten cuidado.";
+    public final static String Alerta2 = " es muy alta, mucho cuidado.";
+    public final static String Alerta3 = " no slagas de la casa, sin bloqueador y agua.";
+    
+    protected void setup (){
+        addBehaviour(new CyclicBehaviour(){
+        public void action(){
+            ACLMessage mensaje =  receive();
+            if(mensaje != null){
+                String msg = mensaje.getContent();
+                int temperatura = Integer.parseInt(msg);
+                if(temperatura >= 35){
+                    ACLMessage nuevoMensaje = new ACLMessage(ACLMessage.INFORM);
+                    nuevoMensaje.setContent(temperatura<=40? "La temperatura es " + msg + Alerta1 :(temperatura <=45 ?"La temperatura es " + msg +Alerta2 : "La temperatura es " + msg +Alerta3));
+                    nuevoMensaje.addReceiver(new AID("Ag_interfaz", AID.ISLOCALNAME));
+                    send(nuevoMensaje);
+                }
+            }else{
+                block();
+            }
+        }    
+        });
+    }
 }
